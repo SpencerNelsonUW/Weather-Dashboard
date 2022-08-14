f52be585901f9a85e6b1b745f659c96e3f496d6d
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-08-14 11:29:52 -0700

    formatted html and started on JS

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+var searchHistory = [];
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = 'dc9fed23d14a1f7f13a170ed656895f3';
+
+
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
+
